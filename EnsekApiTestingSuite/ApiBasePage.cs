5610be69acb64a455c57210da4a872f918cec620
jenna <jenna.lee10021993@gmail.com>
2024-08-14 09:08:20 +0100

    Added project for API test and started to setup base for api pages

---
+++ EnsekApiTestingSuite/ApiBasePage.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp.Authenticators;
+
+namespace EnsekApiTestingSuite
+{
+    public class ApiBasePage
+    {
+        public static IRestClient CreateClient(string baseUrl)
+        {
+
+            // Creates a client using the options object
+            var options = new RestClientOptions(baseUrl)
+            {
+                Timeout = TimeSpan.FromSeconds(1000), 
+                Authenticator = new JwtAuthenticator(baseUrl),
+            };
+            var client = new RestClient(options);
+            return client;
+        }
+    }
+}
---
+++ EnsekApiTestingSuite/BuyApiModel.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+
+namespace EnsekApiTestingSuite
+{
+    public class BuyApiModel: ApiBasePage
+
+    {
+        public BuyApiModel()
+        {
+
+        }
+
+        public RestRequest BuyEnergyRequest()
+        {
+            CreateClient("https://qacandidatetest.ensek.io");
+            RestRequest request = new RestRequest("/ENSEK/buy/1/5335'", Method.Put);
+            var response = request;
+
+            return response;
+        }
+    }
+}
